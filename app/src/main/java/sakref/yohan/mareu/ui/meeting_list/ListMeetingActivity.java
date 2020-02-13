863e1d697a8e2af520361d02d0907cf2b71fbbd3
Sakrefyohan <yohan.sakref@gmail.com>
2020-02-13 19:02:03 +0100

    Using Butterknif for the toolbar

--- app/src/main/java/sakref/yohan/mareu/ui/meeting_list/ListMeetingActivity.java
+++ app/src/main/java/sakref/yohan/mareu/ui/meeting_list/ListMeetingActivity.java
@@ -2,13 +2,12 @@ package sakref.yohan.mareu.ui.meeting_list;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
 import android.os.Bundle;
 
 import android.os.Bundle;
 import android.widget.TextView;
 //import android.widget.Toolbar;
-
-
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -18,12 +17,11 @@ import sakref.yohan.mareu.R;
 
 public class ListMeetingActivity extends AppCompatActivity {
 
-    //@BindView(R.id.toolbar)
-    //Toolbar mToolbar;
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
 
     //@BindView(R.id.activity_meeting_list_add_meeting)
     //FloatingActionButton mFloatingActionButton;
-
 
 
     @Override
@@ -32,14 +30,12 @@ public class ListMeetingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_meeting_list);
         ButterKnife.bind(this);
 
-        Toolbar mToolbar = findViewById(R.id.toolbar);
+
+        //Toolbar mToolbar = findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
 
 
-
     }
-
-
 
 
 }
