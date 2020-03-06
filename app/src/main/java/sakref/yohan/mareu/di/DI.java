f4b228da4c600a08974df961ed4574f746532a8c
Sakrefyohan <yohan.sakref@gmail.com>
2020-03-06 18:24:41 +0100

    Adding file :
    - Meeting Fragment
    - DummyMeetingApiService
    - DI

---
+++ app/src/main/java/sakref/yohan/mareu/di/DI.java
@@ -0,0 +1,35 @@
+package sakref.yohan.mareu.di;
+
+import sakref.yohan.mareu.service.DummyMeetingApiService;
+import sakref.yohan.mareu.service.MeetingApiService;
+
+public class DI {
+
+    /**
+     * Dependency injector to get instance of services
+     */
+
+    private static MeetingApiService service = new DummyMeetingApiService();
+
+    /**
+     * Get an instance on @{@link MeetingApiService}
+     *
+     * @return
+     */
+    public static MeetingApiService getMeetingApiService() {
+        return service;
+    }
+
+    /**
+     * Get always a new instance on @{@link MeetingApiService}. Useful for tests, so we ensure the context is clean.
+     *
+     * @return
+     */
+    public static MeetingApiService getNewInstanceApiService() {
+        return new DummyMeetingApiService();
+    }
+
+    public static MeetingApiService getNeighbourApiService() {
+        return service;
+    }
+}
---
+++ app/src/main/java/sakref/yohan/mareu/service/DummyMeetingApiService.java
@@ -0,0 +1,33 @@
+package sakref.yohan.mareu.service;
+
+import java.util.List;
+
+import sakref.yohan.mareu.model.Meeting;
+import sakref.yohan.mareu.model.Room;
+
+public class DummyMeetingApiService implements MeetingApiService {
+    @Override
+    public List<Meeting> getMeeting() {
+        return null;
+    }
+
+    @Override
+    public void addMeeting(Meeting meeting) {
+
+    }
+
+    @Override
+    public void deleteMeeting(Meeting meeting) {
+
+    }
+
+    @Override
+    public List<Meeting> getFilteredMeeting(String date, String room) {
+        return null;
+    }
+
+    @Override
+    public List<Room> getRoom() {
+        return null;
+    }
+}
