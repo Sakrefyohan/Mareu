7db7b4fb0ff67c40607a0dbfdecd59493fb59e5b
Sakrefyohan <yohan.sakref@gmail.com>
2020-04-02 15:27:00 +0200

    finish : RoomApiService. Review : none To finish : Dialog filter, Test, Display of the filters. Correction : non

--- app/src/main/java/sakref/yohan/mareu/service/RoomApiService.java
+++ app/src/main/java/sakref/yohan/mareu/service/RoomApiService.java
@@ -1,4 +1,28 @@
 package sakref.yohan.mareu.service;
 
-public class RoomApiService {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import sakref.yohan.mareu.R;
+import sakref.yohan.mareu.model.Room;
+
+public abstract class RoomApiService {
+
+    public static List<Room> DUMMY_ROOMS = Arrays.asList(
+            new Room("Beryllium", R.color.colorRoomBeryllium),
+            new Room("Boron",R.color.colorRoomBoron),
+            new Room("Carbon",R.color.colorRoomCarbon),
+            new Room("Fluorine",R.color.colorRoomFluorine),
+            new Room("Helium",R.color.colorRoomHelium),
+            new Room("Hydrogen",R.color.colorRoomHydrogen),
+            new Room("Lithium",R.color.colorRoomLithium),
+            new Room("Neon",R.color.colorRoomNeon),
+            new Room("Nitrogen",R.color.colorRoomNitrogen),
+            new Room("Oxygen",R.color.colorRoomOxygen)
+    );
+
+    static List<Room> generateRoom() {
+        return new ArrayList<>(DUMMY_ROOMS);
+    }
 }
